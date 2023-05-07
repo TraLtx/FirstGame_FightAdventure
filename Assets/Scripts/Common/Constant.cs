53a512c58e6d681d553a8ac5b0ba8be8c45fe3c4
TraLtx <traltx2000@gmail.com>
2023-05-07 22:11:42 +0700

    optimization code

--- Assets/Scripts/Common/Constant.cs
+++ Assets/Scripts/Common/Constant.cs
@@ -8,8 +8,7 @@ public class Constant
     public const string SCENE_LOADING_TO_SERVER = "LoadingToServer";
     public const string SCENE_LOBBY = "Lobby";
     public const string SCENE_LEVEL_MENU = "LevelMenu";
+
     public const string SCENE_LEVEL_1 = "MainPlay";
-
-    
-
+    public const string SCENE_LEVEL_2 = "Level_2";
 }
